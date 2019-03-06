af15c7e3b9135b074aa161fe65e8b275f9c16f3c
Mr. Bartucz <jobartucz@isd535.org>
2019-03-06 07:44:45 -0600

    Added getters and setters for Question Class

--- app/src/main/java/com/ctech/bartucz/geoquiz/MainActivity.java
+++ app/src/main/java/com/ctech/bartucz/geoquiz/MainActivity.java
@@ -4,17 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank = new Question[] {
+
+      new Question(R.string.question_australia, true),
+      new Question(R.string.question_oceans, true),
+      new Question(R.string.question_mideast, false),
+      new Question(R.string.question_africa, false),
+      new Question(R.string.question_americas, true),
+      new Question(R.string.question_asia, true),
+
+    };
+
+    // the current question being shown
+    private int mCurrentIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get a reference to the Question TextView and set its text to the question at the current index
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        int questionResourceId = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(questionResourceId);
+
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +58,18 @@ public class MainActivity extends AppCompatActivity {
                         R.string.incorrect_toast,
                         Toast.LENGTH_SHORT).show();
             }
+        });
+
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                int newQuestionResourceId = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(newQuestionResourceId);
+            }
+
         });
 
     }
